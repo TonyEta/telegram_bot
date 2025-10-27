ffe71d637893cb5c883bf1ecda6301eb4f60d9f2
MaxShamruk <shamruk01@gmail.com>
2025-10-27 15:51:27 +0300

    add .env.example

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -11,3 +11,15 @@ async def start_cmd(message: types.Message):
 @user_private_router.message(Command('menu'))
 async def echo(message: types.Message):
     await message.answer("Меню:")
+
+@user_private_router.message(Command('about'))
+async def echo(message: types.Message):
+    await message.answer("Опис бота")
+
+@user_private_router.message(Command('payment'))
+async def echo(message: types.Message):
+    await message.answer("Варіанти оплати")
+
+@user_private_router.message(Command('shipping'))
+async def echo(message: types.Message):
+    await message.answer("Варіанти доставки")
