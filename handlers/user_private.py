3e912193e7e0f0401d871b577c900b11c62aa80c
MaxShamruk <shamruk01@gmail.com>
2025-10-27 16:31:37 +0300

    new commit

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,25 +1,32 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
 
 
 user_private_router = Router()
+
+
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
     await message.answer("Привіт, я віртуальний помічник")
 
 @user_private_router.message(Command('menu'))
-async def echo(message: types.Message):
+async def menu_cmd(message: types.Message):
     await message.answer("Меню:")
 
 @user_private_router.message(Command('about'))
-async def echo(message: types.Message):
+async def about_cmd(message: types.Message):
     await message.answer("Опис бота")
 
 @user_private_router.message(Command('payment'))
-async def echo(message: types.Message):
+async def payment_cmd(message: types.Message):
     await message.answer("Варіанти оплати")
 
 @user_private_router.message(Command('shipping'))
-async def echo(message: types.Message):
+async def shipping_cmd(message: types.Message):
     await message.answer("Варіанти доставки")
+
+
+@user_private_router.message(F.text)
+async def test_cmd(message: types.Message):
+    await message.answer("Це магічний фільтр")
--- new_bot.py
+++ new_bot.py
@@ -3,12 +3,12 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-
+from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
-
+from common.bot_cmds_list import private
 
 
 ALLOWED_UPDATES = ['message', 'edited_message']  # фільтр оновлень які прийдуть нашому боту, обмеження типів Update
@@ -24,6 +24,7 @@ dp.include_router(user_private_router)
 async def main():
     print('бот запущено')
     await bot.delete_webhook(drop_pending_updates=True)  # видаляє надіслані в бот повідомлення за час коли бот не був запущений
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
